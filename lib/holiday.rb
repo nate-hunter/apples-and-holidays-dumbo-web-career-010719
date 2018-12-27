6883b58a93741a5aa12c49359c9f5ccf932388de
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 16:34:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday, supply|
     stuff << supply 
-
+  end
 end
 
 
